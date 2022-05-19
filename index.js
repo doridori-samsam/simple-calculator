16219085fdbaf8deb894aa506dc15749038cdd04
Sam <wtawonlife@gmail.com>
2022-05-19 22:01:26 +0900

    enter키 기능 추가 및 결과값 submit 방지 속성 추가

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const btns = document.querySelectorAll(".num, .operator, .punct");
 const display = document.querySelector(".screen");
 const clearBtn = document.querySelector(".clear");
 const resultBtn = document.querySelector(".result");
-
+const inputKey = document.querySelector("input.screen");
 
 
 for(let i=0; i<btns.length; i++){
@@ -11,11 +11,18 @@ for(let i=0; i<btns.length; i++){
     })
 };
 
+
 clearBtn.addEventListener('click', ()=>{
     display.value = '';
 });
+
 
 resultBtn.addEventListener('click', ()=>{
     display.value = eval(display.value);
 });
 
+display.addEventListener('keypress', function(e){
+    if(e.key == 'Enter'){
+        display.value = eval(display.value);
+    }
+});
